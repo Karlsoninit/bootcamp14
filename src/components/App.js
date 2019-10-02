b883cacbdde6ef4a7b73be15b8e539d65a1c1397
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-10-02 11:14:17 +0300

    added default page for new project

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Panel from './Panel/Panel';
-import user from '../user.json';
 
 const App = () => {
   return (
     <>
-      <Panel isOpen user={user} />
+      <p>Work</p>
     </>
   );
 };
--- src/components/User/User.style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Descriptions = styled.p`
-  font-size: 30px;
-  color: green;
-`;
-
-export default Descriptions;
